3420a654eb6061fb56d56a075e2ad124ff1950cf
OoO <1095886384@qq.com>
2023-07-28 17:42:13 +0800

    update

--- Python/test.py
+++ Python/test.py
@@ -1,8 +1,21 @@
-s = "global"
-def outer():
-    # s = "outer"
-    def inner():
-        # s = "inner"
-        print(s)
-    inner()
-outer()
+#encoding=utf-8
+#类的方式创建线程
+from threading import Thread
+from time import sleep
+class MyThread(Thread):
+    def __init__(self,name):
+        Thread.__init__(self)
+        self.name =name
+    def run(self):
+        for i in range(3):
+            print(f"thread:{self.name} :{i}")
+            sleep(1)
+if __name__ == '__main__':
+    print("主线程，start")
+    #创建线程(类的方式)
+    t1 = MyThread('t1')
+    t2 = MyThread('t2')
+    #启动线程
+    t1.start()
+    t2.start()
+    print("主线程，end")
